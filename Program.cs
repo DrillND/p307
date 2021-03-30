7c76c3f3bbf0adb303ceb21d1a1cf71fd20f1052
GAGAAA <81124904+jinsim88@users.noreply.github.com>
2021-03-30 14:38:54 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    //class Fibo
+    //{
+    //    public long Get(int i)
+    //    {
+    //        if (i < 0) { return 0; }
+    //        if (i == 1) { return 1; }
+    //        return Get(i - 2) + Get(i - 1);
+
+    //    }
+    //}
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //    int i;
+            //    Fibo fi = new Fibo();
+            //    for (i = 1; i < 10; i++)
+            //    {
+            //        Console.WriteLine(fi.Get(i));
+            //    }
+            //}
+
+
+            int a = 0;
+            int b = 1;
+            int c;
+       
+
+            for(int i = 0;i<10; i++)
+            {
+                Console.WriteLine(a);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+
+        }
+    }
+}
